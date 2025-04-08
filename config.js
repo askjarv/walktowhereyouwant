b81d0116f0dffd7d403839ae0489cf2a3b64f874
Chris <askjarv@users.noreply.github.com>
2025-04-08 15:01:05 +0100

    Update config.js
    Corrected uri

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.config = {
     // See .github/workflows/deploy.yml for details.
     CLIENT_ID: 'YOUR_CLIENT_ID',
     // The redirect URI must match exactly what you registered in your Fitbit app settings
-    REDIRECT_URI: 'https://askjarv.github.io/walktowhereyouwant/index.html'
+    REDIRECT_URI: 'https://askjarv.github.io/walktowhereyouwant/'
 }; 
